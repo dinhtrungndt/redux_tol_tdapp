ca6f581efb04c2dde29c335e843b6b0a1445308d
kho quản lý <nguyendinhtrung.it@gmail.com>
2024-07-02 21:21:28 +0700

    upDevTools and Selector

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,13 +1,16 @@
 import { Col, Row, Input, Button, Select, Tag } from 'antd';
 import Todo from '../Todo';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addTodo } from '../../redux/actions';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
+import { todoListSelector } from '../../redux/selectors';
 
 export default function TodoList() {
   const [todoName, setTodoName] = useState('');
   const [priority, setPriority] = useState('Medium');
+
+  const todoList = useSelector(todoListSelector);
 
   const dispatch = useDispatch()
 
@@ -20,6 +23,8 @@ export default function TodoList() {
         completed: false
       })
     )
+    setTodoName('');
+    setPriority('Medium');
   }
 
   const handleInputChange = (e) => {
@@ -34,9 +39,14 @@ export default function TodoList() {
   return (
     <Row style={{ height: 'calc(100% - 40px)' }}>
       <Col span={24} style={{ height: 'calc(100% - 40px)', overflowY: 'auto' }}>
-        <Todo name='Learn React' prioriry='High' />
+        {/* <Todo name='Learn React' prioriry='High' />
         <Todo name='Learn Redux' prioriry='Medium' />
-        <Todo name='Learn JavaScript' prioriry='Low' />
+        <Todo name='Learn JavaScript' prioriry='Low' /> */}
+        {
+          todoList.map(todo =>
+        <Todo key={todo.id} name={todo.name} prioriry={todo.prioriry} />
+          )
+        }
       </Col>
       <Col span={24}>
         <Input.Group style={{ display: 'flex' }} compact>
